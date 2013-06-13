f35732b84850ec55fca3ef98e3e414754be1e1b5
Pavel Aslanov <asl.pavel@gmail.com>
2013-06-13 13:37:58 +0400

    [setup] 1.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def package_full(package):
     return packages
 
 setup(name='pretzel',
-      version='1.0',
+      version='1.0.1',
       author='Pavel Aslanov',
       description='Pretzel asynchronous python framework',
       long_description=open('README.md').read(),
